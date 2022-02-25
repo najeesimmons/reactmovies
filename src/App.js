ef442361592158d09084bd70ebee7fd8456e9515
najeesimmons <najee.bryant.simmons@gmail.com>
2022-02-25 11:04:52 -0500

    added getMovie function to app.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,25 @@ function App() {
 
   //State to hold movie data
   const [movie, setMovie] = React.useState(null);
+
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+  
+    // Parse JSON response into a javascript object
+    const data = await response.json();
+    //set the Movie state to the movie
+    setMovie(data);
+  };
+
   // USE OUR COMPONENTS IN APPs RETURNED JSX
+  // We pass the getMovie function as a prop called moviesearch
   return (
     <div className="App">
-      <Form />
+      <Form moviesearch={getMovie} />
       <MovieDisplay />
     </div>
   );
