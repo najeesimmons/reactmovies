8f188d2f9d8e9612713c098380ddaffb81bfeeb3
najeesimmons <najee.bryant.simmons@gmail.com>
2022-02-25 11:01:22 -0500

    add state and state modifier to app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Form from "./components/Form";
 function App() {
   //variable with your apiKey
   const apiKey = "98e3fb1f";
+
+  //State to hold movie data
+  const [movie, setMovie] = React.useState(null);
   // USE OUR COMPONENTS IN APPs RETURNED JSX
   return (
     <div className="App">
